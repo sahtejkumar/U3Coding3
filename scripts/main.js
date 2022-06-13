c33179ad9a2b98fba67dde09a1b4253bdcdca431
SahtejKumar <sahtejkumar97@gmail.com>
2022-06-13 13:08:23 +0530

    commits

--- scripts/main.js
+++ scripts/main.js
@@ -2,33 +2,32 @@
 // money.innerText = localStorage.getItem("amount")
 // let previous = localStorage.getItem("amount")
 // let add = () => {
-//     let input = document.querySelector("#amount").value 
+//     let input = document.querySelector("#amount").value
 //     total = eval(+input + +previous)
 //     money.textContent = total
 //     localStorage.setItem("amount", total)
 // }
-document.querySelector("#signup").addEventListener("submit", forsubmit)
-var User = JSON.parse(localStorage.getItem("user")) || []
+document.querySelector("#signup").addEventListener("submit", forsubmit);
+var User = [];
 function forsubmit() {
-    event.preventDefault()
-    
-    var username = document.querySelector("#name").value
-    var useremail = document.querySelector("#email").value
-    var useraddress = document.querySelector("#address").value
-    var useramount = document.querySelector("#amount").value
+  event.preventDefault();
 
-    var info = new FormData(username, useremail, useraddress, useramount)
-    User.push(info)
-    localStorage.setItem("user", JSON.stringify(User))
-    window.location.reload
- }
+  var username = document.querySelector("#name").value;
+  var useremail = document.querySelector("#email").value;
+  var useraddress = document.querySelector("#address").value;
+  var useramount = document.querySelector("#amount").value;
 
+  var info = new FormData(username, useremail, useraddress, useramount);
+  User.push(info);
+  localStorage.setItem("user", JSON.stringify(User));
+  window.location.reload;
+}
 
-class FormData{
-    constructor(N, E, AD, A) {
-        this.name = N
-        this.email = E
-        this.address = AD
-        this.amount=A
-     }
- }
+class FormData {
+  constructor(N, E, AD, A) {
+    this.name = N;
+    this.email = E;
+    this.address = AD;
+    this.wallet = A;
+  }
+}
--- scripts/voucher.js
+++ scripts/voucher.js
@@ -1,42 +1,58 @@
-var url = `https://masai-vouchers-api.herokuapp.com/api/vouchers`
-
-async function fetch() {
-    var url = ` https://masai-vouchers-api.herokuapp.com/api/vouchers`;
-    var res = await fetch(url);
-    var Data = await res.json();
-    var info = Data[0].vouchers
-    Forappend(info)
+var url = `https://masai-vouchers-api.herokuapp.com/api/vouchers`;
+let wallet = JSON.parse(localStorage.getItem("user"));
+console.log(wallet);
+let balance = wallet[0].wallet;
+document.querySelector("#wallet_balance").innerText = balance;
+// console.log(balance)
+async function Data() {
+  var url = ` https://masai-vouchers-api.herokuapp.com/api/vouchers`;
+  var res = await fetch(url);
+  var Data = await res.json();
+  console.log(Data[0].vouchers);
+  // var info = Data[0].voucher
+  // console.log(info)
+  Forappend(Data[0].vouchers);
 }
-Fetch()
+Data();
 
-function Fetch(info) {
-    info.forEach(function (ele){
-        var innerdiv = document.createElement("div");
-        
-        var img = document.createElement("img");
-        img.setAttribute("src", ele.image)
-        
-        var name = document.createElement("h3");
-        name.innerText = ele.name
-        
-        var price = document.createElement("button");
-        price.innerText = ele.price
-        
-        var purchase = document.createElement("button");
-        purchase.innerText = "Buy"
-        purchase.addEventListener("click", function () {
-            var price = ele.price
-            BUY(price)
-        })
-        innerdiv.append(img, name, price, purchase)
-        document.querySelector("#voucher_list").append(innerdiv)
-    })
+function Forappend(info) {
+  info.forEach(function (ele) {
+    var innerdiv = document.createElement("div");
+    innerdiv.setAttribute("class", "voucher");
+    var img = document.createElement("img");
+    img.setAttribute("src", ele.image);
+
+    var name = document.createElement("h3");
+    name.innerText = ele.name;
+
+    var price = document.createElement("button");
+    price.innerText = ele.price;
+
+    var purchase = document.createElement("button");
+    purchase.innerText = "Buy";
+    purchase.addEventListener("click", function () {
+      BUY(ele);
+    });
+    innerdiv.append(img, name, price, purchase);
+    document.querySelector("#voucher_list").append(innerdiv);
+  });
 }
 
-function BUY(price) {
-    var wallet_balance = document.querySelector("#wallet_balance");
-    wallet_balance.innerHTML = null
-    
-    var Amount = JSON.parse(localStorage.getItem("user"))
-    console.log(Amount)
+function BUY(ele) {
+  // var wallet_balance = document.querySelector("#wallet_balance");
+  // wallet_balance.innerHTML = null
+  // console.log(price)
+  // var Amount = JSON.parse(localStorage.getItem("user"))
+  // console.log(Amount)
+  // console.log(ele)
+  if (Number(ele.price) > balance) {
+    alert("Sorry!insufficient balance ");
+  } else {
+    alert("Hurray! purchase successful");
+    balance = balance - Number(ele.price);
+    console.log(balance);
+    document.querySelector("#wallet_balance").innerText = balance;
+    wallet[0].wallet = balance;
+    localStorage.setItem("user", JSON.stringify(wallet));
+  }
 }
